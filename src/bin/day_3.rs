32bb2301c4a8da8f651428801d2f326171c55e66
Helena Recasens <me@helena.re>
2024-12-16 18:38:57 +0100

    Day 3, part 2
    Didn't make it solve both parts at once :(

--- src/bin/day_3.rs
+++ src/bin/day_3.rs
@@ -4,11 +4,26 @@ use regex::Regex;
 
 fn main() {
   let binding = fs::read_to_string("input/day_3.txt").expect("mlem");
-  let mul_re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+  let mul_re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))").unwrap();
   let matches = mul_re.captures_iter(&binding);
-  let total: usize = matches.map(|mul| {
-    let (_, [num1, num2]): (&str, [&str; 2]) = mul.extract();
-    num1.parse::<usize>().unwrap()*num2.parse::<usize>().unwrap()
-  }).sum();
+
+  let mut flag = true;
+  let total: usize = matches
+    .map(|mul| {
+      let num1 = mul.get(1).map_or("", |m| m.as_str());
+      let num2 = mul.get(2).map_or("", |m| m.as_str());
+      let do_word = mul.get(3).map_or("", |m| m.as_str());
+      let dont_word = mul.get(4).map_or("", |m| m.as_str());
+
+      if !do_word.is_empty() {flag = true};
+      if !dont_word.is_empty() {flag = false};
+
+      if flag && !num1.is_empty() && !num2.is_empty() {
+        num1.parse::<usize>().unwrap() * num2.parse::<usize>().unwrap()
+      } else {
+        0
+      }
+    })
+    .sum();
   dbg!(total);
 }
